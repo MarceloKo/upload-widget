1df2bb3fc5b04c7462ba8b8ab7ce3b3890374809
Marcelo <marceloakohlhase@gmail.com>
2025-01-26 18:56:34 -0400

    Storage Cloudflare R2

--- backend/src/app/functions/upload-image.ts
+++ backend/src/app/functions/upload-image.ts
@@ -2,6 +2,7 @@ import { Readable } from 'node:stream'
 import { db } from '@/infra/database'
 import { schema } from '@/infra/database/schemas'
 import { type Either, makeLeft, makeRight } from '@/infra/shared/either'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
 import { z } from 'zod'
 import { InvalidFileFormat } from './errors/invalid-file-format'
 
@@ -22,10 +23,17 @@ export async function uploadImage(
     return makeLeft(new InvalidFileFormat())
   }
 
+  const { key, url } = await uploadFileToStorage({
+    folder: 'images',
+    fileName,
+    contentType,
+    contentStream,
+  })
+
   await db.insert(schema.uploads).values({
     name: fileName,
-    remoteKey: fileName,
-    remoteUrl: fileName,
+    remoteKey: key,
+    remoteUrl: url,
   })
 
   return makeRight({ url: '' })
