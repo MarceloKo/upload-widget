f792aa1fdbf56c66d557b127f2636f57025649da
Marcelo <marceloakohlhase@gmail.com>
2025-01-26 11:28:11 -0400

    configuração com fastify

--- backend/src/infra/http/server.ts
+++ backend/src/infra/http/server.ts
@@ -1,9 +1,33 @@
-import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
+import { env } from '@/env'
+import { uploadImageRoute } from '@/infra/http/routes/upload-image'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 server.register(fastifyCors, { origin: '*' })
 
-server.listen({ port: 3333, host: '0.0.0.0' }, () => {
-    console.log('Server is running on port 3333')
+server.register(uploadImageRoute)
+console.log(env.DATABASE_URL)
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP Server running!')
 })
