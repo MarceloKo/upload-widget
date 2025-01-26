181a22e070bd288d022d0487c7e46c9a868ecdc0
Marcelo <marceloakohlhase@gmail.com>
2025-01-26 11:33:26 -0400

    Swagger

--- backend/src/infra/http/server.ts
+++ backend/src/infra/http/server.ts
@@ -2,10 +2,14 @@ import { env } from '@/env'
 import { uploadImageRoute } from '@/infra/http/routes/upload-image'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import {
   hasZodFastifySchemaValidationErrors,
   serializerCompiler,
   validatorCompiler,
+  jsonSchemaTransform,
 } from 'fastify-type-provider-zod'
 
 const server = fastify()
@@ -24,6 +28,20 @@ server.setErrorHandler((error, request, reply) => {
   return reply.status(500).send({ message: 'Internal server error.' })
 })
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 console.log(env.DATABASE_URL)
